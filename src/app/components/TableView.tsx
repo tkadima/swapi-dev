fb6c2ddc300d2cb92353fac76771433517a9afdd
tkadima <tabithakadima@gmail.com>
2024-09-02 20:14:20 -0400

    Handle onclick on table

--- src/app/components/TableView.tsx
+++ src/app/components/TableView.tsx
@@ -1,4 +1,7 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import router from 'next/router';
+import React, { MouseEvent } from 'react';
+
 type TableViewProps = {
   columns: GridColDef[]
   rows: any[]
@@ -8,6 +11,13 @@ const TableView = ({ title, rows, columns }: TableViewProps) => {
   function getRowId(row: any) {
     return row.url.split('/').slice(-2, -1)[0]
   }
+
+  const handleRowClick = (rowData: any, e: MouseEvent<HTMLDivElement>) => {
+    const resource = rowData.row.url.split('/').slice(-3, -1)[0];
+    console.log('clicked row', rowData.row.url.split('/').slice(-3, -1));
+
+    router.push(`/${resource}/${rowData.id}`);
+  };
 
   return (
     <div>
@@ -20,6 +30,7 @@ const TableView = ({ title, rows, columns }: TableViewProps) => {
           pagination: { paginationModel: { pageSize: 10 } },
         }}
         pageSizeOptions={[10, 25, 50, 100]}
+        onRowClick={(row, e: any) => handleRowClick(row, e)}
       />
     </div>
   )
