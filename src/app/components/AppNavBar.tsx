0a35c1a4c4a2453fe568cb06cfe6d25e5548eebe
tkadima <tabithakadima@gmail.com>
2024-10-15 12:12:33 -0400

    Fix nav bar

--- src/app/components/AppNavBar.tsx
+++ src/app/components/AppNavBar.tsx
@@ -1,25 +1,29 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material'
-
-import React from 'react'
+import { AppBar, Button, IconButton, Toolbar, Typography, Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 type AppNavBarProps = {
   activePage: string
 }
 
-const pages = ['films', 'people', 'vehicles', 'starships', 'planets', 'species']
+const pages = ['films', 'people']
+const dropdownPages = ['vehicles', 'starships', 'planets', 'species']
 
-// Navigation bar for the application that has links to all the the pages of the app
 const AppNavBar = ({ activePage }: AppNavBarProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleDropdownOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleDropdownClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Star Wars
           </Typography>
@@ -28,15 +32,39 @@ const AppNavBar = ({ activePage }: AppNavBarProps) => {
         {pages.map((page, index) => (
           <Link key={index} href={'/' + page} passHref>
             <Button
-              color={
-                activePage.startsWith('/' + page) ? 'secondary' : 'inherit'
-              }
+              color={activePage.startsWith('/' + page) ? 'secondary' : 'inherit'}
               sx={{ fontSize: '1rem' }}
             >
               {page.toLowerCase()}
             </Button>
           </Link>
         ))}
+
+        {/* Dropdown for more pages */}
+        <Button
+          color="inherit"
+          onClick={handleDropdownOpen}
+          sx={{ fontSize: '1rem' }}
+        >
+          More
+        </Button>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleDropdownClose}
+        >
+          {dropdownPages.map((page, index) => (
+            <Link key={index} href={'/' + page} passHref>
+              <MenuItem
+                onClick={handleDropdownClose}
+                selected={activePage.startsWith('/' + page)}
+              >
+                {page.toLowerCase()}
+              </MenuItem>
+            </Link>
+          ))}
+        </Menu>
       </Toolbar>
     </AppBar>
   )
